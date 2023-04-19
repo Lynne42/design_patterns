a01cf6d6e1a54b0952525dddb9b6ee00e1073b52
qlniu <46595240+qlniu@users.noreply.github.com>
2023-04-19 18:04:57 +0800

    1 file modified

--- src/Iterator/example/profileIterator.ts
+++ src/Iterator/example/profileIterator.ts
@@ -1,8 +1,16 @@
 import type Profile from './profile';
 
 interface ProfileIterator {
+  // 获取下一个元素
   getNext() : Profile | null;
 
+  // 获取下一个元素
+  // getPrev() : Profile | null;
+
+  // 获取当前迭代的元素位置
+  // getCurrentPosition(): number;
+
+  // 判断是否有更多元素
   hasMore(): boolean;
 }
 
